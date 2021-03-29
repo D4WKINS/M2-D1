86ecd4d9477212ad8bf256fbd68c9eae09eab88a
D4WKINS <78683271+D4WKINS@users.noreply.github.com>
2021-03-29 15:53:32 +0100

    Update Files

--- script.js
+++ script.js
@@ -6,13 +6,15 @@ const createTapestry =  function(){
         // `<img src="cover-1.webp" class="album-cover">
         let tapestry = document.getElementById('tapestry');
         let div = document.createElement('div')
-        div.classList.add(' col-md-4 col-lg-3')
+        div.classList.add('col-md-4')
+        div.classList.add('col-lg-3')
         let img = document.createElement('img')
         img.setAttribute('src','cover-1.webp')
         img.classList.add('album-cover')
 
-        let cover = ` <div class="col-md-4 col-lg-3 "> ${img} </div>`
-        tapestry.appendChild(cover)
+       
+        tapestry.appendChild(div)
+        div.appendChild(img)
     }
 }
 
