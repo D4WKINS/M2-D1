f42f0dfe427490758f05f27d11590643f3a441dc
D4WKINS <78683271+D4WKINS@users.noreply.github.com>
2021-04-04 07:31:46 +0100

    Update

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ const createTapestry =  function(){
         // `<img src="cover-1.webp" class="album-cover">
         let tapestry = document.getElementById('tapestry');
         let div = document.createElement('div')
-        div.classList.add('col-md-4')
-        div.classList.add('col-lg-3')
+        div.classList.add('col-md-4', 'col-lg-3','mb-5')
         let img = document.createElement('img')
         img.setAttribute('src','cover-1.webp')
         img.classList.add('album-cover')
@@ -21,23 +20,23 @@ const createTapestry =  function(){
 createTapestry()
 
 
-const albumInfo = () =>{
-    let overlay = document.getElementsByClassName('album-overlay')
-    let overlayP = document.getElementsByClassName('overlayP')
-    let display = 'off';
-    for(let i =0; i<overlayP.length; i++){
-        if( display == 'on'){
-            display = 'off'
-            overlayP[i].style.display = "none"
-            overlay[i].classList.remove("album-info")
-            overlay[i].classList.remove("h-100")
-            overlay[i].classList.remove("w-100")
-        }
-        else if(display == 'off')
-        display = 'on'
-        overlayP[i].style.display = "block"
-        overlay[i].classList.add("album-info")
-        overlay[i].classList.add("h-100")
-        overlay[i].classList.add("w-100")
-    }
-}
+// const albumInfo = () =>{
+//     let overlay = document.getElementsByClassName('album-overlay')
+//     let overlayP = document.getElementsByClassName('overlayP')
+//     let display = 'off';
+//     for(let i =0; i<overlayP.length; i++){
+//         if( display == 'on'){
+//             display = 'off'
+//             overlayP[i].style.display = "none"
+//             overlay[i].classList.remove("album-info")
+//             overlay[i].classList.remove("h-100")
+//             overlay[i].classList.remove("w-100")
+//         }
+//         else if(display == 'off')
+//         display = 'on'
+//         overlayP[i].style.display = "block"
+//         overlay[i].classList.add("album-info")
+//         overlay[i].classList.add("h-100")
+//         overlay[i].classList.add("w-100")
+//     }
+// }
